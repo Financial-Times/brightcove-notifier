d970df7960001a0acfe62b508d740340c94f46d2
Zoltan Bodor <Zoltan.Bodor@ft.com>
2016-04-07 16:32:43 +0300

    added handlers skeleton

--- app.go
+++ app.go
@@ -3,8 +3,14 @@ package main
 import (
 	"io"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/jawher/mow.cli"
 )
 
 const dateLayout = time.RFC3339Nano
@@ -18,16 +24,53 @@ func init() {
 	initLogs(os.Stdout, os.Stdout, os.Stderr)
 }
 
+type brightcoveNotifier struct {
+	port        int
+	cmsNotifier string
+}
+
 func main() {
 	app := cli.App("brightcove-notifier", "Gets notified about Brightcove FT video events, creates UPP publish event and posts it to CMS Notifier.")
+	port := app.Int(cli.IntOpt{
+		Name:   "port",
+		Value:  8080,
+		Desc:   "application port",
+		EnvVar: "PORT",
+	})
 	cmsNotifier := app.String(cli.StringOpt{
 		Name:   "cms-notifier",
 		Value:  "localhost:13080",
-		Desc:   "CMS notifier's address",
+		Desc:   "cms notifier's address",
 		EnvVar: "CMS_NOTIFIER",
 	})
 
+	bn := &brightcoveNotifier{*port, *cmsNotifier}
+
+	app.Action = func() {
+		go bn.listen()
+		ch := make(chan os.Signal)
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+		<-ch
+	}
+	app.Run(os.Args)
 }
+
+func (bn brightcoveNotifier) listen() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", bn.handleNotification).Methods("POST")
+	r.HandleFunc("/__health", bn.health).Methods("GET")
+	r.HandleFunc("/__gtg", bn.gtg).Methods("GET")
+
+	http.Handle("/", r)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		errorLogger.Panicf("Couldn't set up HTTP listener: %+v\n", err)
+	}
+}
+
+func (bn brightcoveNotifier) handleNotification(w http.ResponseWriter, r *http.Request) {}
+func (bn brightcoveNotifier) health(w http.ResponseWriter, r *http.Request)             {}
+func (bn brightcoveNotifier) gtg(w http.ResponseWriter, r *http.Request)                {}
 
 func initLogs(infoHandle io.Writer, warnHandle io.Writer, errorHandle io.Writer) {
 	infoLogger = log.New(infoHandle, "INFO  - ", logPattern)
